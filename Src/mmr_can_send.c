8433245ca8f727518490cd6817e84ebbe977c5de
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-12-06 21:02:30 +0100

    Refactor

--- Src/mmr_can_send.c
+++ Src/mmr_can_send.c
@@ -4,8 +4,9 @@
 
 static HalStatus sendNormal(CanHandle *hcan, CanTxHeader *header, MmrCanPacket *packet);
 static HalStatus sendMulti(CanHandle *hcan, CanTxHeader *header, MmrCanPacket *packet);
-static uint8_t computeFramesToSend(uint8_t length);
-static uint8_t computeNextMessageLength(uint8_t totalLength, uint8_t offset);
+static HalStatus sendSingleMultiFrame(CanHandle *hcan, CanTxHeader *header, MmrCanPacket *packet);
+static uint8_t computeFramesToSend(MmrCanPacket *packet);
+static uint8_t computeNextMessageLength(MmrCanPacket *packet, uint8_t offset);
 
 
 HalStatus MMR_CAN_Send(CanHandle *hcan, MmrCanPacket packet) {
@@ -41,18 +42,14 @@ static HalStatus sendMulti(
 ) {
   HalStatus status = HAL_OK;
   uint8_t offset = 0;
-  uint8_t framesToSend = computeFramesToSend(packet->length);
+  uint8_t framesToSend = computeFramesToSend(packet);
 
   header->StdId |= MMR_CAN_MESSAGE_TYPE_MULTI_FRAME;
   do {
-    uint8_t *dataStart = packet->data + offset;
-    uint8_t length = computeNextMessageLength(packet->length, offset);
-
-    header->DLC = length;
-    offset += length;
-
-    status |= HAL_CAN_AddTxMessage(hcan, header, dataStart, packet->mailbox);
-  } while (
+    status |=
+      sendSingleMultiFrame(hcan, header, packet, &offset);
+  }
+  while (
     framesToSend --> 1 && status == HAL_OK
   );
 
@@ -60,13 +57,32 @@ static HalStatus sendMulti(
 }
 
 
-static uint8_t computeFramesToSend(uint8_t length) {
+static HalStatus sendSingleMultiFrame(
+  CanHandle *hcan,
+  CanTxHeader *header,
+  MmrCanPacket *packet,
+  uint8_t *offset
+) {
+  uint8_t *dataStart = packet->data + offset;
+  uint8_t length = computeNextMessageLength(packet, *offset);
+
+  header->DLC = length;
+  *offset += length;
+
+  return HAL_CAN_AddTxMessage(hcan, header, dataStart, packet->mailbox);
+}
+
+
+static uint8_t computeFramesToSend(MmrCanPacket *packet) {
+  uint8_t length = packet->length;
   uint8_t framesToSend = length / MMR_CAN_MAX_DATA_LENGTH;
   uint8_t maybeOneForRemainder = length % MMR_CAN_MAX_DATA_LENGTH > 0;
 
   return framesToSend + maybeOneForRemainder;
 }
 
-static uint8_t computeNextMessageLength(uint8_t totalLength, uint8_t offset) {
-  return min(totalLength - offset, MMR_CAN_MAX_DATA_LENGTH);
+static uint8_t computeNextMessageLength(MmrCanPacket *packet, uint8_t offset) {
+  return min(
+    packet->length - offset, MMR_CAN_MAX_DATA_LENGTH
+  );
 }
